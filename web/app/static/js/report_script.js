d0cb32fe71157c9d95da935bbb71ec1e39aca377
Fabrício Dalvi Venturim <fabriciodalviventurim@gmail.com>
2024-10-15 21:56:51 -0300

    add doc

--- web/app/static/js/report_script.js
+++ web/app/static/js/report_script.js
@@ -1,35 +1,35 @@
+/**
+ * Class representing a UI handler for various user interface interactions.
+ */
 class UIHandler {
     constructor() {
-        // Elements related to machine information
         this.destinationRadios = document.getElementsByName('destination');
         this.machineInfo = document.getElementById('machineInfo');
         this.machineNumberInput = document.getElementById('machineNumber');
-        
-        // Elements related to filter toggling
         this.filterSection = document.getElementById('filtersSection');
         this.toggleButton = document.getElementById('toggleFilters');
-
-        // Elements related to machine filter
         this.filterTarget = document.getElementById('filterTarget');
         this.filterMachineContainer = document.getElementById('filterMachineContainer');
-        
-        // Initialize the functionalities
         this.initDestinationHandler();
         this.initToggleFilters();
         this.initMachineFilterHandler();
     }
 
-    // Initializes the destination selection handler
+    /**
+     * Initializes the destination selection handler.
+     */
     initDestinationHandler() {
         this.destinationRadios.forEach(radio => {
             radio.addEventListener('change', this.handleRadioChange.bind(this));
         });
     }
 
-    // Handles the change of selection of the radio buttons
+    /**
+     * Handles the change of selection of the radio buttons.
+     * @param {Event} event - The event triggered by changing a radio button.
+     */
     handleRadioChange(event) {
         const selectedRadio = event.target;
-
         if (selectedRadio.value === 'machine' && selectedRadio.checked) {
             this.showMachineInfo();
         } else if (selectedRadio.value === 'app' && selectedRadio.checked) {
@@ -37,41 +37,49 @@ class UIHandler {
         }
     }
 
-    // Shows the machine information section
+    /**
+     * Shows the machine information section.
+     */
     showMachineInfo() {
         this.machineInfo.style.display = 'block';
         this.machineNumberInput.setAttribute('required', 'required');
     }
 
-    // Hides the machine information section
+    /**
+     * Hides the machine information section.
+     */
     hideMachineInfo() {
         this.machineInfo.style.display = 'none';
         this.machineNumberInput.removeAttribute('required');
     }
 
-    // Initializes the filter toggle button
+    /**
+     * Initializes the filter toggle button.
+     */
     initToggleFilters() {
         this.toggleButton.addEventListener('click', this.toggleFilters.bind(this));
     }
 
-    // Toggles the visibility of the filter section
+    /**
+     * Toggles the visibility of the filter section.
+     */
     toggleFilters() {
         const isHidden = this.filterSection.classList.toggle('hidden');
         this.filterSection.classList.toggle('visible', !isHidden);
-
         this.toggleButton.innerHTML = isHidden ? '<i class="fas fa-filter"></i> Show Filters' : '<i class="fas fa-filter"></i> Hide Filters';
     }
 
-    // Initializes the machine filter handler
+    /**
+     * Initializes the machine filter handler.
+     */
     initMachineFilterHandler() {
-        // Calls toggleMachineFilter to set the initial state
         this.toggleMachineFilter();
-
-        // Adds event listener to handle changes in the 'filterTarget' dropdown
         this.filterTarget.addEventListener('change', this.toggleMachineFilter.bind(this));
     }
 
-    // Toggles the visibility of the machine filter based on the selected target
+    /**
+     * Toggles the visibility of the machine filter based on the selected target.
+     */
     toggleMachineFilter() {
         if (this.filterTarget.value === 'machine') {
             this.filterMachineContainer.style.display = 'block';
@@ -81,7 +89,6 @@ class UIHandler {
     }
 }
 
-// Initialize the UIHandler when the DOM is fully loaded
 document.addEventListener('DOMContentLoaded', () => {
     new UIHandler();
 });
