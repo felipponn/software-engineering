26c60c09dd8cc26cf620a158de70b1bf31680480
Otávio Augusto <otavio.oam@protonmail.com>
2024-11-04 13:46:08 -0300

    simplify machine initialization and create method to get machine profile

--- backend/machine.py
+++ backend/machine.py
@@ -8,14 +8,6 @@ class Machine:
     Attributes:
     - machine_id: int
         The unique identifier of the machine. 
-    - location: str
-        The location of the machine.
-    - status: str
-        The status of the machine. If the machine is operational or not.
-    - last_serviced_at: datetime
-        The date and time when the machine was last serviced.
-    - installed_at: datetime
-        The date and time when the machine was installed.
         
     Methods:
     - __init__(self, machine_id: int, location: str, products: list,
@@ -23,49 +15,106 @@ class Machine:
         Constructor to initialize the Machine object.
     - get_machines()
         Static method to return a list of Machine objects from the database.
+    - get_profile()
+        Method to get all the informations about the machine
     """
     
-    def __init__(self, location: str, status: str, last_serviced_at: datetime,
-                 installed_at: datetime, machine_id: int = None):
+    def __init__(self, machine_id: int = None):
         """
         Constructor to initialize the Machine object.
 
         Attributes:
         - machine_id: int
             The unique identifier of the machine.
-        - location: str
-            The location of the machine.
-        - status: str
-            The status of the machine. If the machine is operational or not.
-        - last_serviced_at: datetime
-            The date and time when the machine was last serviced.
-        - installed_at: datetime
-            The date and time when the machine was installed.
         """
         self.machine_id = machine_id
-        self.location = location
-        self.status = status
-        self.last_serviced_at = last_serviced_at
-        self.installed_at = installed_at
 
     @staticmethod
     def get_machines():
         """
-        Static method to return a list of Machine objects from the database.
+        Static method to return a list of Machine ids from the database.
 
         Returns:
         - list
             The list of Machine objects.
         """
         query = """
-                SELECT machine_id, location, status, last_serviced_at, installed_at
+                SELECT machine_id
                 FROM Coffee_Machines;
                 """
         machines_data = execute_query_fetchall(query)
         
-        machines = []
-        for machine_data in machines_data:
-            machine_id, _, _, _, _ = machine_data
-            machines.append(machine_id)
+        return machines_data
 
-        return machines
+    def get_profile(self):
+        """
+        Method to get all the informations about the machine.
+
+        returnz;
+        -list
+            The list of atributes of the machine.
+        """
+        profile_query = f"""
+                SELECT * FROM Coffee_Machines
+                WHERE machine_id = {self.machine_id};
+                """
+        machine_profile = execute_query_fetchone(profile_query)
+
+        available_products_query = f"""
+                SELECT p.name, p.price
+                FROM Products p
+                JOIN Coffee_Machine_Products cmp ON p.product_id = cmp.product_id
+                WHERE cmp.machine_id = {self.machine_id} AND cmp.quantity > 0;
+                """
+        available_products = execute_query_fetchall(available_products_query)
+
+        machine_reviews_query =f"""
+                SELECT r.review_id, u.name AS user_name, r.rating, r.comment, r.created_at
+                FROM Reviews r
+                JOIN Users u ON r.user_id = u.user_id  -- Join with Users to get the reviewer’s name
+                WHERE r.machine_id = {self.machine_id};  -- Replace <machine_id> with the actual machine ID you want to query
+                """
+        machine_reviews = execute_query_fetchall(machine_reviews_query)
+
+        reviews_info = self.post_process_reviews(machine_reviews)
+
+        return machine_profile, available_products, reviews_info
+    
+
+    @staticmethod
+    def post_process_reviews(reviews):
+        """
+        Post-processes the list of reviews to calculate the average rating,
+        count the total number of reviews, and filter out reviews without comments.
+
+        Args:
+            reviews (list): A list of tuples containing reviews data.
+
+        Returns:
+            dict: A dictionary containing the mean rating, count of all reviews, and filtered reviews.
+        """
+        # Initialize total rating and count of reviews
+        total_rating = 0
+        total_reviews_count = len(reviews)
+
+        # Calculate total rating including reviews without comments
+        for review in reviews:
+            total_rating += review[2]  # Assume review[2] is the rating
+
+        # Calculate the mean rating
+        mean_rating = total_rating / total_reviews_count if total_reviews_count > 0 else 0
+
+        # Filter reviews that have comments
+        filtered_reviews = [review for review in reviews if review[3] is not None and review[3].strip() != '']
+
+        num_filtered_reviews = len(filtered_reviews)
+
+        # Prepare result
+        result = {
+            'mean_rating': mean_rating,
+            'count_reviews': total_reviews_count,
+            'filtered_reviews': filtered_reviews,
+            'num_filtered_reviews': num_filtered_reviews,
+        }
+        
+        return result
